d0b4a885e484a4ab9ff1584d9d73ffdfd63f78a4
Alessandro Polidori <alessandro.polidori@gmail.com>
2015-06-29 22:36:37 +0200

    add icons, old notifications & home links. Fixes #7 #5

--- js/popup.js
+++ js/popup.js
@@ -29,20 +29,66 @@ var getNotificaitons = function(discourseUrl) {
 };
 
 var appendUrl = function(discourseUrl, n) {
-  n.url = discourseUrl + '/t/' + n.slug + '/' + n.topic_id + '/' + n.post_number;
+  if (!n.data.badge_id) {
+    n.url = discourseUrl + '/t/' + n.slug + '/' + n.topic_id + '/' + n.post_number;
+  } else {
+    n.url = discourseUrl + '/badges/' + n.data.badge_id + '/' + n.data.badge_name.replace(/[^A-Za-z0-9_]+/g, '-').toLowerCase();
+  }
   return n;
 };
+
+var iconClassMap = {
+  2:  'fa-reply',
+  5:  'fa-heart',
+  9:  'fa-reply',
+  12: 'fa-certificate'
+}
+
+var extractData = function (n) {
+  n.popNot = { icon: iconClassMap[n.notification_type] };
+  if (!n.data.badge_id) {
+    n.popNot.key = n.data.display_username;
+    n.popNot.value = n.data.topic_title;
+  } else {
+    n.popNot.key = 'Earned';
+    n.popNot.value = '\'' + n.data.badge_name + '\'';
+  }
+  return n;
+}
 
 var template = function(n) {
   var classes = 'notification js-notification-link';
   if (!n.read) classes += ' unread';
   return [
     '<a href="' + n.url + '" class="' + classes + '">',
+      '<i class="fa ' + n.popNot.icon + '"></i>',
       '<span class="notification-username">',
-        n.data.display_username,
+        n.popNot.key,
       '</span>',
       '<span class="notification-title">',
-        n.data.topic_title,
+        n.popNot.value,
+      '</span>',
+    '</a>'
+  ].join('');
+};
+
+var homeTemplate = function(discourseUrl) {
+  var classes = 'notification js-notification-link';
+  return [
+    '<a href="' + discourseUrl + '" class="' + classes + '">',
+      '<span class="notification-title">',
+        'Home',
+      '</span>',
+    '</a>'
+  ].join('');
+};
+
+var olderTemplate = function(discourseUrl) {
+  var classes = 'notification js-notification-link';
+  return [
+    '<a href="' + discourseUrl + '/my/notifications" class="' + classes + '">',
+      '<span class="notification-title">',
+        'view older notifications...',
       '</span>',
     '</a>'
   ].join('');
@@ -67,9 +113,12 @@ chrome.storage.sync.get({ discourseUrl: '' }, function (items) {
 
   getNotificaitons(discourseUrl).then(function(data) {
 
-      var html = data.notifications.map(appendUrl.bind(null, discourseUrl))
+      var html = homeTemplate(discourseUrl);
+      html += data.notifications.map(appendUrl.bind(null, discourseUrl))
+                                   .map(extractData)
                                    .map(template)
                                    .join('');
+      html += olderTemplate(discourseUrl);
 
       document.querySelector('.js-notifications').innerHTML = html;
       initLinks();
